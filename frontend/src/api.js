81bb192fcacd8c30899c28603136c03ccc7091cf
ASURAMVP <kaushikdnath201@gmail.com>
2025-10-15 23:39:58 +0530

    Add API functions for file upload and management

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:4000/api'
+});
+
+export async function requestUploadUrl({ filename, contentType, size, userId }) {
+  const resp = await API.post('/upload-url', { filename, contentType, size, userId });
+  return resp.data;
+}
+
+export async function confirmUpload({ uploadId, size }) {
+  const resp = await API.post('/confirm-upload', { uploadId, size });
+  return resp.data;
+}
+
+export async function listFiles() {
+  const resp = await API.get('/files');
+  return resp.data;
+}
+
+export async function deleteFile(id) {
+  const resp = await API.delete(`/files/${id}`);
+  return resp.data;
+}
